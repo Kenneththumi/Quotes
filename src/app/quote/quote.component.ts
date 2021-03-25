6fe0bb33cda9df3eeb2831ec287e78ec8032c490
Kenneth Thumi <kenmwangi4237@gmail.com>
2021-03-25 23:58:41 +0300

    Voting  enhanced

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -13,6 +13,7 @@ export class QuoteComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  //Quotes initial array
   quotes:Quote[] = [
         new Quote('Do what you can, with what you have, where you are.','Theodore Roosevelt','Thumi',new Date(2020,6,1)),
         new Quote('Dont let yesterday take up too much of today.','Will Rogers','Thumi',new Date(2019,6,1))
@@ -22,5 +23,16 @@ export class QuoteComponent implements OnInit {
   //display/hide quote details
   toggleDetails(index:number){
        this.quotes[index].moreDetails = !this.quotes[index].moreDetails;
+  }
+
+  //upvoting
+  upvote(index:number){
+      this.quotes[index].vote += 1;
+      console.log(this.quotes[index].vote)
+  }
+
+  //downvoting
+  downvote(index:number){
+      this.quotes[index].vote -= 1;
   }
 }
